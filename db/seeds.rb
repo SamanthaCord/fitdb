00fe3d3720afd8c235a85f23c26aa2386385b3f1
Sam Cordingley <sam@cordandberg.com>
2018-03-18 19:34:14 +1100

    added exercise seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Exercise.destroy_all
+
+#  name        :text
+#  description :text
+#  category    :integer
+#  equipment   :integer
+#  image       :text
+#  video       :text
+
+e1 = Exercise.create :name => 'Lunges', :description => 'Lunges on both sides. Repeat for 30 seconds.', :category => 2, :equipment => 0
+
+e2 = Exercise.create :name => 'Push ups', :description => 'High to low push ups. Repeat for 30 seconds.', :category => 1, :equipment => 0
+
+e3 = Exercise.create :name => 'Rowing', :description => 'Use rowing machine. Repeat for 30 seconds.', :category => 1, :equipment => 3
+
+e4 = Exercise.create :name => 'Bike Riding', :description => 'Ride the bike for 30 seconds.', :category => 2, :equipment => 2
+
+e5 = Exercise.create :name => 'Burpees', :description => 'Burpees. Repeat for 30 seconds.', :category => 1, :equipment => 0
+
+e6 = Exercise.create :name => 'High knees', :description => 'Running on the spot with arms out in front, hit knees on hands. Repeat for 30 seconds.', :category => 3, :equipment => 0
+
+e7 = Exercise.create :name => 'Arm raises', :description => 'Raise arms out to the side and back down, keeping arms straight. Repeat for 30 seconds.', :category => 1, :equipment => 1
+
+e8 = Exercise.create :name => 'Star Jumps', :description => 'Star jumps. Repeat for 30 seconds.', :category => 2, :equipment => 0
+
+e9 = Exercise.create :name => 'Sit ups', :description => 'Full sit ups. Repeat for 30 seconds.', :category => 3, :equipment => 0
+
+e10 = Exercise.create :name => 'Plank', :description => 'Hold full or half plank for 30 seconds.', :category => 4, :equipment => 0
+
+Workout.destroy_all
+
+w1 = Workout.create
+
+w2 = Workout.create
+
+w1.exercises << e1
+w1.exercises << e3
+w1.exercises << e9
+
+w2.exercises << e10
+w2.exercises << e2
+w2.exercises << e6
