ce4f91ada837f6ea607d7aed74eb1f5a98f0fb51
Sam Cordingley <sam@cordandberg.com>
2018-03-18 20:48:25 +1100

    randomly select exercises

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   def home
     @exercises = Exercise.all
   end
+
+  def daily
+    exercises = Exercise.all.pluck(:name, :description, :category, :equipment)
+    @firstExercise = exercises.sample
+    @secondExercise = exercises.sample
+    @thirdExercise = exercises.sample
+  end
 end
